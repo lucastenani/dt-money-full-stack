1f2a0b699d6e5575dd1742a07073007638aa16ee
lucastenani <lucas.tenani3@gmail.com>
2024-08-26 17:45:05 -0300

    feat - Create initial Knex migration to test functionality

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,13 @@
-import { knex as setupKnex } from 'knex'
+import { Knex, knex as setupKnex } from 'knex'
 
-export const knex = setupKnex({
+export const config: Knex.Config = {
   client: 'sqlite',
-  connection: { filename: './tmp/app.db' },
-})
+  connection: { filename: './db/app.db' },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
