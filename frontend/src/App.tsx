f18895ad1cac86d96215574eb1d523cd2003cb92
lucastenani <lucas.tenani3@gmail.com>
2024-08-27 15:16:37 -0300

    feat - Implement dark/light theme toggle for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import './globals.css'
 
 import { Header } from './components/header'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
-    <div className="bg-background min-h-screen w-screen font-sans">
-      <Header />
+    <ThemeProvider defaultTheme="dark" storageKey="dt-money-theme">
+      <div className="bg-card min-h-screen w-screen font-sans">
+        <Header />
 
-      <main></main>
-    </div>
+        <main></main>
+      </div>
+    </ThemeProvider>
   )
 }
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,21 +1,23 @@
 import logoPath from '@/assets/logo.svg'
 
+import { ThemeToggle } from './theme/theme-toggle'
 import { Button } from './ui/button'
 
 export function Header() {
   return (
-    <header className="bg-background w-screen py-12">
-      <div className="container flex w-full items-center justify-between md:w-2/3">
+    <header className="bg-background w-screen py-12 lg:py-20">
+      <nav className="container flex w-full items-center justify-between md:w-2/3">
         <img
           className="w-full max-w-28 md:max-w-36"
           src={logoPath}
           alt="DT Money Logo"
         />
 
-        <div className="flex items-center justify-center gap-4 md:gap-8">
+        <div className="ml-auto flex items-center justify-center gap-2 md:gap-4">
+          <ThemeToggle />
           <Button>New transaction</Button>
         </div>
-      </div>
+      </nav>
     </header>
   )
 }
