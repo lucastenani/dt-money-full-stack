fb4879972868558b94aee32074075bf0525edc56
lucastenani <lucas.tenani3@gmail.com>
2024-09-03 17:01:48 -0300

    chore - Add permissions for frontend-backend integration in production

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,10 +3,9 @@ import axios from 'axios'
 import { env } from '@/env'
 
 export const api = axios.create({
-  withCredentials: true,
   baseURL: env.VITE_API_URL,
+  withCredentials: true, 
   headers: {
-    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
   },
 })
