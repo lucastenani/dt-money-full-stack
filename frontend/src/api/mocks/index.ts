afda169ae31bbf3759427aba97c865051a549ebd
lucastenani <lucas.tenani3@gmail.com>
2024-09-05 17:45:21 -0300

    test - Add test mocks using MSW

--- frontend/src/api/mocks/index.ts
+++ frontend/src/api/mocks/index.ts
@@ -3,10 +3,7 @@ import { setupWorker } from 'msw/browser'
 import { env } from '@/env'
 
 import { getSummaryMock } from './get-summary-mock'
-import {
-  getTransactionsMock,
-  getTransactionsMock,
-} from './get-transactions-mock'
+import { getTransactionsMock } from './get-transactions-mock'
 import { registerTransactionMock } from './register-transaction-mock'
 
 export const worker = setupWorker(
